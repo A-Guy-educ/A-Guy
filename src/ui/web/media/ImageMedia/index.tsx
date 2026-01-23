541eced62f11ff429292aa332f56959157fa8574
aguyaharonyair <aguy.aharon.yair@gmail.com>
2026-01-23 22:40:53 +0200

    refactor: Move utility files to infra directory
    - Move cssVariables.js to src/ui/web/media/cssVariables.ts
    - Move environment.d.ts to src/infra/types/environment.d.ts
    - Move instrumentation-client.ts to src/infra/instrumentation-client.ts
    - Update ImageMedia component import

--- src/ui/web/media/ImageMedia/index.tsx
+++ src/ui/web/media/ImageMedia/index.tsx
@@ -8,8 +8,8 @@ import React from 'react'
 
 import type { Props as MediaProps } from '../types'
 
-import { cssVariables } from '@/cssVariables'
 import { getMediaUrl } from '@/infra/utils/getMediaUrl'
+import { cssVariables } from '../cssVariables'
 
 const { breakpoints } = cssVariables
 
